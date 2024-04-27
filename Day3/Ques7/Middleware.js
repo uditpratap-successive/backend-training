5158e854a0c97dc6a3947b1d9b93711f6431269a
Udit Pratap <udit.pratap@uditpratapr-425.successivetech.com>
2024-04-27 13:40:30 +0530

    assignment day-3

---
+++ Day3/Ques7/Middleware.js
@@ -0,0 +1,52 @@
+
+
+const validation = require("./Config");
+const middleware = (req, res, next) => {
+  const route = req.path;
+  console.log(route);
+  const method = req.method;
+  console.log(method);
+
+  if (validation[route] && validation[route][method]) {
+    const rules = validation[route][method];
+    console.log(rules);
+    if (method == "POST") {
+      for (const [fieldName, fieldRules] of Object.entries(rules)) {
+        console.log(fieldName);
+        if (fieldRules.required && !req.body[fieldName]) {
+          return res.status(400).json({ error: `${fieldName} is required` });
+        }
+        if (
+          fieldRules.minLength &&
+          req.body[fieldName].length < fieldRules.minLength
+        ) {
+          return res
+            .status(400)
+            .json({
+              error: `${fieldName} must be at least ${fieldRules.minLength} characters long`,
+            });
+        }
+      }
+    } else if (method == "GET") {
+      for (const [fieldName, fieldRules] of Object.entries(rules)) {
+        console.log(fieldName);
+        if (fieldRules.required && !req.query[fieldName]) {
+          return res.status(400).json({ error: `${fieldName} is required` });
+        }
+       else if (
+          fieldRules.minLength &&
+          req.query[fieldName].length < fieldRules.minLength
+        ) {
+          return res
+            .status(400)
+            .json({
+              error: `${fieldName} must be at least ${fieldRules.minLength} characters long`,
+            });
+        }
+      }
+    }
+  }
+  next();
+};
+
+module.exports = middleware;
