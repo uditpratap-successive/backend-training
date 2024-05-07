8425121dfdd96bf8a5d09b1cb801fca44e628db8
Udit Pratap <udit.pratap@uditpratapr-425.successivetech.com>
2024-05-08 00:38:20 +0530

    assignment day-8

---
+++ Day8/SchemaModal.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose');
+
+const countryschema=new mongoose.Schema({
+    country:String,
+    state:Number
+})
+const collection=mongoose.model('countries',countryschema);
+module.exports=collection;
